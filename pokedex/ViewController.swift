7c457c7eab81f7c59e5b10b64f1d0a4b80f49fbe
Russ Johnson <russ@angry-fly.com>
2016-03-13 23:10:42 -0400

    Added background music and audio player controls. Main label and some cleanup on UI

--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -7,23 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
   @IBOutlet weak var collection: UICollectionView!
+  
+  var pokemon = [Pokemon]()
+  var musicPlayer: AVAudioPlayer!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     collection.delegate = self
     collection.dataSource = self
+    
+    initAudio()
+    parsePokemonCSV()
+  }
+  
+  func initAudio() {
+    let path = NSBundle.mainBundle().pathForResource("music", ofType: "mp3")!
+    
+    do {
+      musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(string: path)!)
+      musicPlayer.numberOfLoops = -1
+      musicPlayer.play()
+    } catch let err as NSError {
+        print(err.debugDescription)
+    }
+  }
+  
+  func parsePokemonCSV(){
+    let path = NSBundle.mainBundle().pathForResource("pokemon", ofType: "csv")!
+    
+    do {
+      let csv = try CSV(contentsOfURL: path)
+      let rows = csv.rows
+      
+      for row in rows {
+        let pokeId = Int(row["id"]!)!
+        let name = row["identifier"]!
+        let poke = Pokemon(name: name, pokedexId: pokeId)
+        pokemon.append(poke)
+      }
+      
+    } catch let err as NSError {
+      print(err.debugDescription)
+    }
   }
   
   func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
     if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell {
       
-      let pokemon = Pokemon(name: "Test", pokedexId: indexPath.row + 1)
-      cell.configureCell(pokemon)
+      let poke = pokemon[indexPath.row]
+      cell.configureCell(poke)
       
       return cell
     } else {
@@ -47,6 +85,15 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     return CGSizeMake(105, 105)
   }
 
+  @IBAction func musicBtnPressed(sender: UIButton!) {
+    if musicPlayer.playing {
+      musicPlayer.stop()
+      sender.alpha = 0.3
+    } else {
+      musicPlayer.play()
+      sender.alpha = 1.0
+    }
+  }
 
 }
 
